5d08f6ccf335cd2f4c50181799aba952419a8ad5
nprimo <primo.niccolo@gmail.com>
2022-04-22 16:38:30 +0100

    First commit

---
+++ inc/parser.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   parser.h                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nprimo <nprimo@student.42lisboa.com>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/04/22 16:24:43 by nprimo            #+#    #+#             */
+/*   Updated: 2022/04/22 16:29:23 by nprimo           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PARSERS_H
+# define PARSERS_H
+
+# include <stdio.h>
+# include <stdlib.h>
+# include <readline/readline.h>
+# include <readline/history.h>
+
+# define PROMPT "$> "
+#endif
