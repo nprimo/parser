9696a540c05ba04548dfb671079b1160f732b779
nprimo <primo.niccolo@gmail.com>
2022-04-28 12:19:46 +0100

    Update main.c

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: nprimo <nprimo@student.42lisboa.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/22 16:24:22 by nprimo            #+#    #+#             */
-/*   Updated: 2022/04/22 17:44:31 by nprimo           ###   ########.fr       */
+/*   Updated: 2022/04/28 11:56:56 by nprimo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,11 +17,11 @@ int	main(void)
 	char 	*input;
 	t_list	*token_list;
 
-	token_list = NULL;	
 	input = readline(PROMPT);
-	token_list = get_token_list(input);
-	printf("%s\n", input);
+	printf("INPUT: %s\n", input);
 	add_history(input);
+	token_list = NULL;
+	token_list = get_token_list(input, &token_list);
 	free(input);
 	return (0);
 }
